8a6260c1cb85fd3e42bcecc29dd9482e4d81024d
Pirisinu <palazzialessandro2@gmail.com>
2024-02-14 12:57:50 +0100

    [ add ] Aside in app

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import Aside from './components/partials/Aside.vue';
+export default {
+  name: 'App',
+  components: {
+    Aside
+  },
+  props: {},
+  data() {
+    return {}
+  },
+  mounted() {},
+  beforeDestroy() {},
+  methods: {},
+}
+</script>
+
+<template>
+ <div class="d-flex h-100 w-100">
+    <Aside />
+    <main>lorem5000</main>
+ </div>
+</template>
+
+<style lang="scss" scoped>
+main{
+  width: calc(100% - 250px);
+}
+</style>
