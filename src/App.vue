709d7c8bb774c1af31b2783801390767aca56f54
Pirisinu <palazzialessandro2@gmail.com>
2024-02-14 16:07:32 +0100

    [ add ] import Aside,Header components in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script>
 import Aside from './components/partials/Aside.vue';
+import Header from './components/partials/Header.vue';
 export default {
   name: 'App',
   components: {
-    Aside
+    Aside,
+    Header
   },
   props: {},
   data() {
@@ -18,12 +20,16 @@ export default {
 <template>
  <div class="d-flex h-100 w-100">
     <Aside />
-    <main>lorem5000</main>
+    <main>
+      <Header />
+      lorem5000
+    </main>
  </div>
 </template>
 
 <style lang="scss" scoped>
 main{
   width: calc(100% - 250px);
+  box-shadow: 10px 20px 40px 20px;
 }
 </style>
