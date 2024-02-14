e3a8616781aa61dc84856906034e7436d031bece
Pirisinu <palazzialessandro2@gmail.com>
2024-02-14 12:56:59 +0100

    [ add ] Header component

---
+++ src/components/partials/Header.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'Header',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  mounted() {},
+  beforeDestroy() {},
+  methods: {}
+}
+</script>
+
+<template>
+  <div></div>  
+</template>
+
+<style lang="scss" scoped></style>
